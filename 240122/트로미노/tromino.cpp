ff0f7be5a3dade8013626608612796904074a015
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-01-22 19:00:05 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/트로미노/tromino.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int grid[200][200];
+
+int block(int i, int j) {
+	int maxi = 0;
+	int sum = 0;
+
+	if (i + 1 < n && j - 1 >= 0) {
+		sum += (grid[i][j] + grid[i + 1][j - 1] + grid[i + 1][j]);
+		maxi = max(maxi, sum);
+		sum = 0;
+	}
+
+	if (i + 1 < n && j + 1 < n) {
+		sum += (grid[i][j] + grid[i + 1][j] + grid[i + 1][j + 1]);
+		maxi = max(maxi, sum);
+		sum = 0;
+	}
+
+	if (i + 1 < n && j + 1 < n) {
+		sum += (grid[i][j] + grid[i + 1][j] + grid[i][j + 1]);
+		maxi = max(maxi, sum);
+		sum = 0;
+	}
+
+	if (i + 1 < n && j + 1 < n) {
+		sum += (grid[i][j] + grid[i][j + 1] + grid[i + 1][j + 1]);
+		maxi = max(maxi, sum);
+		sum = 0;
+	}
+
+	if (i + 2 < n) {
+		sum += (grid[i][j] + grid[i + 1][j] + grid[i + 2][j]);
+		maxi = max(maxi, sum);
+		sum = 0;
+	}
+
+	if (j + 2 < n) {
+		sum += (grid[i][j] + grid[i][j + 1] + grid[i][j + 2]);
+		maxi = max(maxi, sum);
+		sum = 0;
+	}
+
+	return maxi;
+}
+
+int main() {
+	int m;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> grid[i][j];
+		}
+	}
+
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			ans = max(ans, block(i, j));
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
