2ec5d2872d2c187901c20a1a9c4830d1f669ebe0
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-05 10:27:07 +0900

    Title: 단 한 번의 2048 시도, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240205/단 한 번의 2048 시도/one-trial-of-2048-game.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int grid[4][4];
+
+vector<int> move(vector<int> v) {
+	vector<int> temp;
+	for (int i = 0; i < 3; i++) {
+		if ((v[i] != 0) && (v[i] == v[i + 1])) {
+			v[i] = 2 * v[i];
+			v[i + 1] = 0;
+		}
+	}
+
+	int num_zero = 0;
+	for (int i = 0; i < 4; i++) {
+		if (v[i] != 0) {
+			temp.push_back(v[i]);
+		}
+		else {
+			num_zero++;
+		}
+	}
+
+	for (int i = 0; i < num_zero; i++) {
+		temp.push_back(0);
+	}
+
+	return temp;
+}
+
+int main() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> grid[i][j];
+		}
+	}
+
+	char dir; cin >> dir;
+
+	vector<vector<int>> arr;
+
+	if (dir == 'R') {
+		int temp[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				temp[i][j] = grid[i][3 - j];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				grid[i][j] = temp[i][j];
+			}
+		}
+	}
+
+	else if (dir == 'U') {
+		int temp[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				temp[i][j] = grid[j][i];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				grid[i][j] = temp[i][j];
+			}
+		}
+	}
+
+	else if (dir == 'D') {
+		int temp[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				temp[i][j] = grid[j][3 - i];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				grid[i][j] = temp[i][j];
+			}
+		}
+	}
+
+
+
+	for (int i = 0; i < 4; i++) {
+		vector<int> v;
+		int num_zero = 0;
+		for (int j = 0; j < 4; j++) {
+			if (grid[i][j] != 0) {
+				v.push_back(grid[i][j]);
+			}
+			else {
+				num_zero++;
+			}
+		}
+		for (int j = 0; j < num_zero; j++) {
+			v.push_back(0);
+		}
+		arr.push_back(v);
+	}
+
+	for (int i = 0; i < 4; i++) {
+		vector<int> temp;
+		arr[i] = move(arr[i]);
+	}
+
+	if (dir == 'R') {
+		int temp[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				temp[i][j] = arr[i][3 - j];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				arr[i][j] = temp[i][j];
+			}
+		}
+	}
+
+	else if (dir == 'U') {
+		int temp[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				temp[i][j] = arr[j][i];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				arr[i][j] = temp[i][j];
+			}
+		}
+	}
+
+	else if (dir == 'D') {
+		int temp[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				temp[i][j] = arr[j][3 - i];
+			}
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				arr[i][j] = temp[i][j];
+			}
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << '\n';
+	}
+}
