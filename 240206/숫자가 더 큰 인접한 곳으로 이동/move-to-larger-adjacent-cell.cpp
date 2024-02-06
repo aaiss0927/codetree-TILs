be952c4306216e2f76a95e66f736d9588c8c0e91
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-06 10:28:48 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240206/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int n, r, c;
+int grid[100][100];
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
+bool is_range(int x, int y) {
+	return (x >= 0 && x < n && y >= 0 && y < n);
+}
+
+int main() {
+	cin >> n >> r >> c;
+	r--; c--;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> grid[i][j];
+		}
+	}
+
+	int c_x = c;
+	int c_y = r;
+	cout << grid[c_y][c_x] << ' ';
+
+	while (true) {
+		bool flag = false;
+
+		for (int i = 0; i < 4; i++) {
+			int n_x = c_x + dx[i];
+			int n_y = c_y + dy[i];
+
+			if (grid[n_y][n_x] > grid[c_y][c_x]) {
+				c_x = n_x;
+				c_y = n_y;
+				flag = true;
+				cout << grid[c_y][c_x] << ' ';
+				break;
+			}
+		}
+
+		if (!flag) {
+			break;
+		}
+	}
+}
