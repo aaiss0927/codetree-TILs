89c2b7eddce128061607c6339251797bcb015b10
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-07 21:37:47 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240207/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int n, m, t;
+int grid[20][20];
+int num_marble[20][20];
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+int main() {
+	cin >> n >> m >> t;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> grid[i][j];
+		}
+	}
+
+	while (m--) {
+		int r, c; cin >> r >> c;
+		r--; c--;
+
+		num_marble[r][c]++;
+	}
+
+	while (t--) {
+		int num_next_marble[20][20];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				num_next_marble[i][j] = num_marble[i][j];
+			}
+		}
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (num_marble[i][j] != 0) {
+					int max = grid[i][j];
+					pair<int, int> pos_max = { i, j };
+
+					for (int k = 0; k < 4; k++) {
+						int n_x = i + dx[k];
+						int n_y = j + dy[k];
+
+						if (grid[n_x][n_y] > max) {
+							max = grid[n_x][n_y];
+							pos_max = { n_x, n_y };
+						}
+					}
+
+					num_next_marble[i][j]--;
+					num_next_marble[pos_max.first][pos_max.second]++;
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (num_next_marble[i][j] > 1) {
+					num_next_marble[i][j] == 0;
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				num_marble[i][j] = num_next_marble[i][j];
+			}
+		}
+	}
+
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (num_marble[i][j] == 1) {
+				ans++;
+			}
+		}
+	}
+
+	cout << ans;
+}
