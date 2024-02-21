cfce40af62c02d340ef8914b2fd94f2d214bc457
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-21 19:14:31 +0900

    Title: 그래프 탐색, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240221/그래프 탐색/graph-traversal.cpp
+++ 240221/그래프 탐색/graph-traversal.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int n, m;
-// n: v_num, m: e_num
-int graph[1001][1001] = {0, };
-bool visited[1001] = {false, };
-
 int cnt = 0;
+vector<vector<int>> graph;
+vector<bool> visited;
 
 void DFS(int vertex) {
-    for (int i = 1; i <= n; i++) {
-        if (graph[vertex][i] && !visited[i]) {
+    for (int i = 0; i < graph[vertex].size(); i++) {
+        int cur_v = graph[vertex][i];
+        if (!visited[cur_v]) {
             cnt++;
-            visited[i] = true;
-            DFS(i);
+            visited[cur_v] = true;
+            DFS(cur_v);
         }
     }
 }
@@ -21,10 +21,14 @@ void DFS(int vertex) {
 int main() {
     cin >> n >> m;
 
+    graph = vector<vector<int>>(n + 1, vector<int>());
+    visited = vector<bool>(n + 1, false);
+
     for (int i = 0; i < m; i++) {
         int s, e; cin >> s >> e;
 
-        graph[s][e] = graph[e][s] = 1;
+        graph[s].push_back(e);
+        graph[e].push_back(s);
     }
 
     visited[1] = true;
