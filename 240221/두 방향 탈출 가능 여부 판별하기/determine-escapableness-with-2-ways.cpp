bc376468cee7afa332599eecce33dd6786512fbe
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-21 20:33:46 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240221/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240221/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int n, m;
-int is_snake[100][100] = {0, };
-bool visited[100][100] = {false, };
 int dx[2] = {1, 0};
 int dy[2] = {0, 1};
+int grid[100][100];
+bool visited[100][100] = {false, };
 
 bool is_range(int x, int y) {
-    return (x >= 0 && x < n && y >= 0 && y < n);
+    return (x >= 0 && x < n && y >= 0 && y < m);
 }
 
 bool can_go(int x, int y) {
@@ -20,7 +20,7 @@ bool can_go(int x, int y) {
         return false;
     }
 
-    if (is_snake[x][y] == 0) {
+    if (grid[x][y] == 0) {
         return false;
     }
 
@@ -33,7 +33,7 @@ void DFS(int x, int y) {
         int n_y = y + dy[i];
 
         if (can_go(n_x, n_y)) {
-            visited[n_x][n_y] = 1;
+            visited[n_x][n_y] = true;
             DFS(n_x, n_y);
         }
     }
@@ -44,7 +44,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> is_snake[i][j];
+            cin >> grid[i][j];
         }
     }
 
@@ -54,6 +54,7 @@ int main() {
     if (visited[n - 1][m - 1]) {
         cout << 1;
     }
+
     else {
         cout << 0;
     }
