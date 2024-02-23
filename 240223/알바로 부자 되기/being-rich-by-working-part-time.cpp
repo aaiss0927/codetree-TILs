fb14469cc3f65931007a1781dc41fa22fea2bb00
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-23 19:46:29 +0900

    Title: 알바로 부자 되기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240223/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 240223/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 int n;
+pair<int, int> dp[1000];
 vector<tuple<int, int, int>> pt;
 
 bool cmp(tuple<int, int, int> t1, tuple<int, int, int> t2) {
@@ -36,40 +37,29 @@ int main() {
         int s, e, p;
         cin >> s >> e >> p; s--; e--;
         pt.push_back({ s, e, p });
+        dp[i] = {get<1>(pt[i]), get<2>(pt[i])};
     }
 
+    //dp = {last_point, cost}
     sort(pt.begin(), pt.end(), cmp);
 
-    int last_pnt = get<1>(pt.back());
-
-    int* dp = new int[last_pnt + 1];
-
-    for (int i = 0; i <= last_pnt; i++) {
-        dp[i] = 0;
-        int extra = 0;
-
-        for (int j = 0; j < n; j++) {
-            if (get<0>(pt[j]) == 0 && get<1>(pt[j]) == i) {
-                extra = get<2>(pt[j]);
-            }
-        }
-
-        dp[i] += extra;
+    for (int i = 0; i < n; i++) {
+        dp[i] = {get<1>(pt[i]), get<2>(pt[i])};
     }
 
-    for (int i = 1; i <= last_pnt; i++) {
+    for (int i = 1; i < n; i++) {
         for (int j = i - 1; j >= 0; j--) {
             int extra = 0;
 
             for (int k = 0; k < n; k++) {
-                if (get<0>(pt[k]) == j + 1 && get<1>(pt[k]) == i) {
-                    extra = get<2>(pt[k]);
+                if (get<0>(pt[k]) == dp[j].first + 1 && get<1>(pt[k]) == i) {
+                    extra += get<2>(pt[k]);
                 }
             }
 
-            dp[i] = max(dp[i], dp[j] + extra);
+            dp[i].second = max(dp[i].second, dp[j].second + extra);
         }
     }
 
-    cout << dp[last_pnt];
+    cout << dp[n - 1].second;
 }
