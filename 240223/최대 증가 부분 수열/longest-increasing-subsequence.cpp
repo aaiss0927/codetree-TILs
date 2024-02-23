2d49413c847e9fb25ae3877d66e7a5c503694f2d
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-23 17:49:33 +0900

    Title: 최대 증가 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240223/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240223/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -5,49 +5,7 @@ using namespace std;
 
 int n;
 vector<int> arr;
-int ans = 1;
-
-bool is_asc(vector<int> v) {
-    for (int i = 1; i < v.size(); i++) {
-        if (v[i] <= v[i - 1]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-vector<int> v;
-vector<int> new_arr;
-
-bool flag_finish;
-
-void choose(int cnt, int next) {
-    if (flag_finish) {
-        return;
-    }
-
-    if (v.size() == cnt) {
-        new_arr.clear();
-
-        for (int i = 0; i < cnt; i++) {
-            new_arr.push_back(arr[v[i]]);
-        }
-
-        if (is_asc(new_arr)) {
-            ans = cnt;
-            flag_finish = true;
-        }
-
-        return;
-    }
-
-    for (int i = next; i < n; i++) {
-        v.push_back(i);
-        choose(cnt, i + 1);
-        v.pop_back();
-    }
-}
+int dp[1001];
 
 int main() {
     cin.tie(0);
@@ -60,12 +18,20 @@ int main() {
         arr.push_back(num);
     }
 
-    for (int i = 2; i <= n; i++) {
-        v.clear();
-        new_arr.clear();
-        flag_finish = false;
-        choose(i, 0);
+    dp[0] = 1;
+
+    for (int i = 1; i < 1001; i++) {
+        dp[i] = -1;
     }
 
-    cout << ans;
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    sort(dp, dp + n);
+    cout << dp[n - 1];
 }
