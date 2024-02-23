fad4a2b68c7d8da07decba10c94086d5d93aefcb
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-23 17:52:36 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240223/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240223/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -18,10 +18,8 @@ int main() {
         arr.push_back(num);
     }
 
-    dp[0] = 1;
-
-    for (int i = 1; i < 1001; i++) {
-        dp[i] = -1;
+    for (int i = 0; i < 1001; i++) {
+        dp[i] = 1;
     }
 
     for (int i = 1; i < n; i++) {
