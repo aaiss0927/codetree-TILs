31da8a24788db1a38e8d8bc57b2d2bf7fad2ea3c
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-26 23:29:34 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int n;
+long long dp[1001];
+
+int main() {
+    cin >> n;
+
+    dp[0] = 1;
+    dp[1] = 2;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % 1000000007;
+
+        for (int j = i - 3; j >= 0; j--) {
+            dp[i] = (dp[i] + 2 * dp[j]) % 1000000007;
+        }
+    }
+
+    cout << dp[n];
+}
