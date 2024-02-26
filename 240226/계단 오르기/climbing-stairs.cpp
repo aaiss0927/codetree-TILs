794da5ed3bd1941b4f7b80451dad040a07771f24
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-26 22:55:14 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+    cin >> n;
+
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+
+    for (int i = 4; i <= n; i++) {
+        dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
+    }
+
+    cout << dp[n];
+}
