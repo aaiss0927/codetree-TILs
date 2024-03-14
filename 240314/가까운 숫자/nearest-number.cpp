ea7676f695f793ef3a0ae3aa511c8cc974ac7d5a
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-03-14 19:45:09 +0900

    Title: 가까운 숫자, Time: 389ms, Memory: 4MB, Status: Passed - CodeTree

--- 240314/가까운 숫자/nearest-number.cpp
+++ 240314/가까운 숫자/nearest-number.cpp
@@ -11,23 +11,25 @@ int main() {
 	cin >> n;
 
 	s.insert(0);
+	int sht = INT_MAX;
 
 	for (int i = 0; i < n; i++) {
 		int num; cin >> num;
 		s.insert(num);
 
-		int sht = INT_MAX;
+		set<int>::iterator it = s.find(num);
+		
+		int cand_1 = num - *(--it); ++it;
 
-		for (auto it = s.begin(); it != s.end(); it++) {
-			if (*it == *s.rbegin()) {
-				continue;
-			}
-
-			int first = *it++;
-			int second = *it--;
-			sht = min(second - first, sht);
+		if (*it == *s.rbegin()) {
+			sht = min(sht, cand_1);
+			cout << sht << '\n';
+			continue;
 		}
 
+		int cand_2 = *(++it) - num; --it;
+
+		sht = min(sht, min(cand_1, cand_2));
 		cout << sht << '\n';
 	}
 }
