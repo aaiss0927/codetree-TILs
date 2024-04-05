ad56bbf1027339d52da2e0243846740105a1fa27
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-05 20:18:09 +0900

    Title: 자주 등장한 top k 숫자, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240405/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
+++ 240405/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 using namespace std;
 
-bool cmp(pair<long long, long long> a, pair<long long, long long> b) {
+bool cmp(pair<long long, long long>& a, pair<long long, long long>& b) {
 	if (a.second < b.second) {
 		return true;
 	}
