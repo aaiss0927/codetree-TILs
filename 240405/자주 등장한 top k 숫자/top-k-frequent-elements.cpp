f16e1d988ebd5e14678017fe356bcb9bf978ea35
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-05 20:13:51 +0900

    Title: 자주 등장한 top k 숫자, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240405/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
+++ 240405/자주 등장한 top k 숫자/top-k-frequent-elements.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+	if (a.second < b.second) {
+		return true;
+	}
+
+	else if (a.second == b.second) {
+		if (a.first < b.first) {
+			return true;
+		}
+
+		else {
+			return false;
+		}
+	}
+
+	else {
+		return false;
+	}
+}
 
 int main() {
 	int n, k; cin >> n >> k;
-	vector<long long> v(n);
-	unordered_map<long long, long long> map;
+	vector<int> v(n);
+	unordered_map<int, int> map;
 
 	for (int i = 0; i < n; i++) {
 		cin >> v[i];
 		map[v[i]]++;
 	}
 
-	while (k--) {
-		long long max_count = 0;
-		long long max_elemt = 0;
+	vector<pair<int, int>> temp(map.begin(), map.end());
+	sort(temp.begin(), temp.end(), cmp);
 
-		for (int i = n - 1; i >= 0; i--) {
-			if (map[v[i]] > max_count) {
-				max_count = map[v[i]];
-				max_elemt = v[i];
-			}
-		}
-
-		map[max_elemt] = -1;
-		cout << max_elemt << " ";
+	for (int i = temp.size() - 1; i >= temp.size() - k; i--) {
+		cout << temp[i].first << " ";
 	}
 }
