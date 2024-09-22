1eb6fea0c22e9a11a7e4a0ae2f62c664acc536fc
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-09-22 22:56:01 +0900

    Title: 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240922/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, m; cin >> n >> m;
+	vector<int> coin(n);
+	vector<int> dp(m + 1);
+
+	for (int i = 1; i <= m; i++)
+		dp[i] = INT_MAX;
+
+	for (int i = 0; i < n; i++)
+		cin >> coin[i];
+
+	for (int i = 1; i <= m; i++) {
+		int min = INT_MAX;
+
+		for (int j = 0; j < n; j++) {
+			if (i >= coin[j]) {
+				if (dp[i - coin[j]] < min)
+					min = dp[i - coin[j]];
+			}
+		}
+
+		dp[i] = min + 1;
+	}
+
+	if (dp[m] == INT_MAX)
+		cout << -1;
+	else
+		cout << dp[m];
+}
